7afba5452d5103d04ab69bc4bf0892981a4e39fe
Eric Weaver <eweaver755@gmail.com>
2019-03-20 15:50:07 -0400

    remove unnecessary curator-framework dependency (#25)

--- src/test/java/com/jwplayer/southpaw/util/ZookeeperTestServer.java
+++ src/test/java/com/jwplayer/southpaw/util/ZookeeperTestServer.java
@@ -19,24 +19,17 @@ import kafka.utils.ZKStringSerializer$;
 import kafka.utils.ZkUtils;
 import org.I0Itec.zkclient.ZkClient;
 import org.I0Itec.zkclient.ZkConnection;
-import org.apache.curator.framework.CuratorFramework;
-import org.apache.curator.framework.CuratorFrameworkFactory;
-import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.curator.test.TestingServer;
-import org.apache.curator.framework.imps.CuratorFrameworkState;
 
 import java.io.IOException;
 
 
 public class ZookeeperTestServer {
-    private CuratorFramework zookeeperTestServer;
     private TestingServer testingServer;
 
     public ZookeeperTestServer() {
         try {
             testingServer = new TestingServer();
-            ExponentialBackoffRetry backoffRetry = new ExponentialBackoffRetry(1000, 4);
-            zookeeperTestServer = CuratorFrameworkFactory.newClient(getConnectionString(), backoffRetry);
         } catch (Exception ex) {
             throw new RuntimeException("Couldn't start test ZK server", ex);
         }
@@ -53,9 +46,6 @@ public class ZookeeperTestServer {
 
     public void shutdown() {
         try {
-            if (zookeeperTestServer.getState().equals(CuratorFrameworkState.STARTED)) {
-                zookeeperTestServer.close();
-            }
             testingServer.close();
         } catch (IOException ex) {
             throw new RuntimeException("Couldn't shutdown test ZK server", ex);
