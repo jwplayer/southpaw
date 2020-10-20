7d1cc1f0d830445cf6a8c9edfda8b4ff8b66f7d1
Quentin LeCorre <quentin@jwplayer.com>
2020-10-20 09:36:37 -0400

    Updates

--- src/main/java/com/jwplayer/southpaw/Southpaw.java
+++ src/main/java/com/jwplayer/southpaw/Southpaw.java
@@ -24,7 +24,6 @@ import com.jwplayer.southpaw.index.BaseIndex;
 import com.jwplayer.southpaw.index.MultiIndex;
 import com.jwplayer.southpaw.index.Reversible;
 import com.jwplayer.southpaw.json.*;
-import com.jwplayer.southpaw.json.Record;
 import com.jwplayer.southpaw.record.BaseRecord;
 import com.jwplayer.southpaw.serde.BaseSerde;
 import com.jwplayer.southpaw.state.BaseState;
@@ -142,10 +141,6 @@ public class Southpaw {
      * State for Southpaw
      */
     protected BaseState state;
-    /**
-     * Nb of 309f5c old foreign keys scrubbed when cleaning parent indices
-     */
-    protected int nbRemovedOldForeignKeys309f5c;
 
     /**
      * Base Southpaw config
@@ -613,7 +608,6 @@ public class Southpaw {
             Relation root,
             ByteArraySet rootRecordPKs) {
         int nbWrittenDenormalizedRecords = 0;
-        nbRemovedOldForeignKeys309f5c = 0;
         for(ByteArray dePrimaryKey: rootRecordPKs) {
             if(dePrimaryKey != null) {
                 BaseTopic<byte[], DenormalizedRecord> outputTopic = outputTopics.get(root.getDenormalizedName());
@@ -660,13 +654,9 @@ public class Southpaw {
             metrics.denormalizedRecordsToCreateByTopic.get(root.getDenormalizedName())
                     .update(metrics.denormalizedRecordsToCreateByTopic.get(root.getDenormalizedName()).getValue() - 1);
         }
-        if (nbRemovedOldForeignKeys309f5c > 0) {
-            logger.info(String.format("Scrubbed %d 309f5c old foreign keys for user_custom_params child relation in %s parent relation", nbRemovedOldForeignKeys309f5c, root.getEntity()));
-        }
         if (logToInfo) {
             logger.info(String.format("Ordered the creation of %d %s denormalized records", nbWrittenDenormalizedRecords, root.getEntity()));
         }
-        nbRemovedOldForeignKeys309f5c = 0;
     }
 
     /**
@@ -1055,9 +1045,6 @@ public class Southpaw {
                     for(ByteArray oldForeignKey: ImmutableSet.copyOf(oldForeignKeys)) {
                         parentIndex.remove(oldForeignKey, rootPrimaryKey);
                         nbRemovedOldForeignKeys += 1;
-                        if (child != null && child.getEntity().equals("user_custom_params") && oldForeignKey != null && oldForeignKey.toString().equals("309f5c")) {
-                            nbRemovedOldForeignKeys309f5c += 1;
-                        }
                     }
                     if (rootPrimaryKey != null && rootPrimaryKey.toString().equals("309f5c")) {
                         logger.info(String.format("Scrubbed %s parent index for %d old foreign keys", parent.getEntity(), nbRemovedOldForeignKeys));
